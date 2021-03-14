ed1af9004aa21cd8d37e41f415042b818dc442fc
Mathias Fiedler <mathias.fiedler@gmail.com>
2021-03-14 11:22:38 +0100

    Use embassy

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 #![no_std]
 #![no_main]
+#![feature(min_type_alias_impl_trait)]
+#![feature(impl_trait_in_bindings)]
+#![feature(type_alias_impl_trait)]
+#![allow(incomplete_features)]
 
-use cortex_m_rt::entry;
-use defmt::{info};
+use defmt::{info, unwrap};
 use defmt_rtt as _;
+use embassy::executor::Spawner;
+use embassy::time::{Duration, Timer};
 use panic_probe as _;
 
-#[entry]
-fn main() -> ! {
-    info!("Hello, world!");
-
+#[embassy::task]
+async fn periodic_task() {
     loop {
-        cortex_m::asm::wfe();
+        info!("update");
+        Timer::after(Duration::from_secs(1)).await;
     }
+}
+
+#[embassy::main]
+async fn main(spawner: Spawner) {
+    unwrap!(spawner.spawn(periodic_task()));
+
 }
